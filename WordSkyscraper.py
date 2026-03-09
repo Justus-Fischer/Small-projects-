a613176c8daa8ff3a29bbbf80c1b59aabed7239f
Justus-Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-03-09 16:23:22 +0100

    Add files via upload

---
+++ WordSkyscraper.py
@@ -0,0 +1,16 @@
+text = str("YourText" + " ")
+height = int(10) # height of the Building
+
+
+number = height
+sp = number
+number = int(0)
+
+for i in range (sp):
+    print(text * number)
+    number = number + 1
+    
+for i in range (sp):
+    print(text * number)
+    number = number - 1
+
