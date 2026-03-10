19b8efb7faf6291b23cd71a4eb3d0559dd3733bd
Justus-Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-03-10 17:29:24 +0100

    Add files via upload
    Bug Fix

--- WordSkyscraper.py
+++ WordSkyscraper.py
@@ -1,16 +1,32 @@
 text = "YourText"
-height = 10 # height of the Building
+height = 6 # height of the Roof -> Building is 2.5 times as high
+vertical = True # Vertical or Horizontal?
 
 
 
 ftext = str(text + " ")
-number = int(0)
 
-for i in range (height):
-    print(ftext * number)
-    number = number + 1
+if vertical == False:
+    number = int(0)
+    for i in range (height):
+        print(ftext * number)
+        number = number + 1
     
-for i in range (height):
-    print(ftext * number)
-    number = number - 1
+    for i in range (height):
+        print(ftext * number)
+        number = number - 1
+        
+        
+else:
+    length = len(ftext)
+    number = height
+    br = int(1)
+    for i in range(height):
+        print(" " * length * number + ftext * br)
+        number = number - 1
+        br = br + 2
+    for i in range(height + height // 2):
+        print(" " * length * number + ftext * br)
+        
+    
 
