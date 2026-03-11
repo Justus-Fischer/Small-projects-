56810fe7d2ab0863117f6572ac512534e6301c9d
Justus-Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-03-11 11:52:57 +0100

    Add files via upload
    implemented console input + minor improvements

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,6 +1,8 @@
 import random
 word = []
 
+print("How long should the password be? (enter a number)")
+n = int(input())
 
 def bu(num):
     for i in range(num):
@@ -9,6 +11,17 @@ def bu(num):
             sp = chr(random.randint(0, 130))
     
         word.append(sp)
-       
-bu(15)
-print("".join(word))
+
+
+while True:
+    bu(n)
+    print("".join(word))
+    word = []
+    print("Another password? Enter the length to continue or leave with NO")
+    k = str(input())
+    try:
+        n = int(k)
+    except:
+        break
+    
+ 
