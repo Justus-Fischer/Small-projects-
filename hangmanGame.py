cbad053ed4d74db19187daec6b3213ef39e84f27
Justus Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-03-13 09:26:23 +0100

    Add files via upload
    minor improvements

--- hangmanGame.py
+++ hangmanGame.py
@@ -8,23 +8,35 @@ wor = random.choice(worter)
 liwor = list(wor)
 suc = ["_ "] * len(wor)
 fehl = 0
-print("Welcome to the hangman game. What is your first guess (tip in one letter)")
-fer = False
-print(" ".join(suc))
-while fer == False:
-    guess = str(input().upper())
-    if guess in wor:
-        for i in range(len(wor)):
-            if liwor[i] == guess:
-                suc[i] = str(guess)
-            
-        print(" ".join(suc))
-        if not "_" in " ".join(suc):
-            fer = True
-        continue
-    if not guess in wor:
-        fehl = fehl + 1
-    print(" ".join(suc))
+
+while True:
     
-print("Congratulations!")
-print("You had " + str(fehl) + " failed attemps.")
+    print("Welcome to the hangman game. What is your first guess (tip in one letter)")
+    fer = False
+    print(" ".join(suc))
+    while fer == False:
+        guess = str(input().upper())
+        if guess in wor:
+            for i in range(len(wor)):
+                if liwor[i] == guess:
+                    suc[i] = str(guess)
+            
+            print(" ".join(suc))
+            if not "_" in " ".join(suc):
+                fer = True
+            continue
+        if not guess in wor:
+            fehl = fehl + 1
+        print(" ".join(suc))
+    
+    print("Congratulations!")
+    print("You had " + str(fehl) + " failed attemps.")
+    print("Start next round with [YES] or quit with [NO]")
+    guess = str(input().upper())
+    if "N" in guess:
+        break
+    
+    wor = random.choice(worter)
+    liwor = list(wor)
+    suc = ["_ "] * len(wor)
+    fehl = 0
