9594b1e586e81f33bbd3597ccea53d28fa120e78
Justus Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-05-17 15:42:40 +0200

    Add files via upload

--- Crypto_Text.py
+++ Crypto_Text.py
@@ -15,24 +15,28 @@ def genKey():
 
 
 
-def crypto(mes, mode):
+def crypto(mes,mode):
     if mode == 1:
         bmes = list(mes)
         for i in range (len(bmes)):
             bmes[i] = ord(bmes[i])
             bmes[i] = chr(bmes[i] + key)
-            
-        return "".join(bmes)
+    else:
+        bmes = list(mes)
+        for i in range (len(bmes)):
+            bmes[i] = ord(bmes[i])
+            bmes[i] = chr(bmes[i] - key)
+    return "".join(bmes)
             
 
     
- print("Welcome to Crypto_Text!")  
+print("Welcome to Crypto_Text!")  
 
 
 while True:
     print("Do you want to Encrypt or Decrypt? (En/De)")
     if "EN" in input().upper():
-        print('Choose your key (every number possible) or type "generate" for a key which is saver')
+        print('Choose your key (a number bigger than 0) or type "generate" for a key which is saver')
         sp = input()
         if "GEN" in sp.upper():
             key = genKey()
@@ -45,10 +49,19 @@ while True:
         
         print("Your encrypted message is: ")
         print(" ")
-        print(str(crypto(mes, 1)))
+        print(str(crypto(mes,1)))
         print(" ")
         print("Please don't forget your Key: " + str(key))
             
         
         
-    #else:
+    else:
+        print("Type in/Paste the encrypted message")
+        mes = str(input())
+        print("Type in the Key")
+        key = int(input())
+        print(" ")
+        print("The decrypted message is: ")
+        print(" ")
+        print(str(crypto(mes,2)))
+        print(" ")
