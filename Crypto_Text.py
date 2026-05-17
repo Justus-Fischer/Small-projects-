894faf2dd6dd6fc854a85d2496cd81c745200dc3
Justus Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-05-17 15:12:35 +0200

    Add files via upload

--- Crypto_Text.py
+++ Crypto_Text.py
@@ -1,8 +1,27 @@
+import random
+int key
+int VZ
+
+def genKey():
+    if random.randint(1, 0) == 1:
+        VZ = 1
+    else:
+        VZ = 0
+        
+    key = andom.randint(1, 50)
+    if VZ == 0:
+        key = 0 - key
 print("Welcome to Crypto_Text!")
 
 while True:
     print("Do you want to Encrypt or Decrypt? (En/De)")
     if "EN" in input().upper():
+        print("Choose your key (every number possible) or type generate for a key which is saver")
+        try:
+            key = int(input())
+        except:
+            key = genKey()
+            
         
         
     else:
